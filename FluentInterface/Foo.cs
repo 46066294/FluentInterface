24ec466c420a75435de2f27ef9bc842544a87cbc
Marc <46066294p@iespoblenou.org>
2016-04-04 13:10:09 +0200

    second commit

---
+++ FluentInterface/Foo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentInterface
+{
+    public class Foo
+    {
+        private string _field;
+        private string _where;
+
+        public Foo Select(string fields)
+        {
+            _field = fields;
+            return this;
+        }
+
+        public Foo Where(string where)
+        {
+            _where = where;
+            return this;
+        }
+
+        public string Execute()
+        {
+            return "Select " + _field + " Where " + _where;
+        }
+
+    }
+
+    
+}
---
+++ FluentInterface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FluentInterface
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var foo = new Foo().Select("lolo").Where("lala").Execute();
+
+            Console.WriteLine(foo);
+        }
+    }
+}
